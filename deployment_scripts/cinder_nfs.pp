4efb55ad83789a6618e2e8b9b80d82ae58bf9826
root <root@fuel.domain.tld>
2016-03-25 15:34:19 +0000

    Significant changes

--- deployment_scripts/cinder_nfs.pp
+++ deployment_scripts/cinder_nfs.pp
@@ -1,11 +1,19 @@
 notice('MODULAR: cinder_nfs.pp')
+$cinder_nfs = hiera('cinder_nfs', {})
+$server_ip = pick($cinder_nfs['server_ip'], {})
+$share_name = pick($cinder_nfs['share_name'], {})
 
 
+
+file { '/etc/cinder':
+	ensure => 'directory',
+	}
 file { '/etc/cinder/nfsshares':
 	ensure => 'present',
-	content => '!HOST:SHARE!',
+	content => "$server_ip:$share_name",
 	owner => 'root',
 	group => 'cinder',
 	mode => '0640',
+	require => File['/etc/cinder'],
 	}
 
