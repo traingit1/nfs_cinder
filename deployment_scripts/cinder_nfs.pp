6e2036791e2f1262c1710eba26be09508380f3a0
root <root@fuel.domain.tld>
2016-03-25 16:05:45 +0000

    Add file_line

--- deployment_scripts/cinder_nfs.pp
+++ deployment_scripts/cinder_nfs.pp
@@ -2,9 +2,11 @@ notice('MODULAR: cinder_nfs.pp')
 $cinder_nfs = hiera('cinder_nfs', {})
 $server_ip = pick($cinder_nfs['server_ip'], {})
 $share_name = pick($cinder_nfs['share_name'], {})
+$cinder = [ 'cinder-api', 'cinder-scheduler']
 
-
-
+service { $cinder:
+	ensure => 'running',
+}
 file { '/etc/cinder':
 	ensure => 'directory',
 	}
@@ -16,4 +18,15 @@ file { '/etc/cinder/nfsshares':
 	mode => '0640',
 	require => File['/etc/cinder'],
 	}
-
+file_line { 'enable_nfs':
+	line => "nfs_shares_config = /etc/cinder/nfsshare",
+	after => "[DEFAULT]",
+	path => "/etc/cinder/cinder.conf",
+	notify => Service[$cinder],
+	}
+file_line { 'cinder_driver':
+	line => "volume_driver = cinder.volume.drivers.nfs.NfsDriver",
+	after => "nfs_shares_config",
+	path => "/etc/cinder/cinder.conf",
+	notify => Service[$cinder],
+	}
